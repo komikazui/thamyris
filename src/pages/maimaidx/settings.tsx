a5972c774721802f7327ec3ea11c28f7eed14d1c
Polaris <tmillerklugman@gmail.com>
2025-04-01 18:04:28 -0400

    oops

--- src/pages/maimaidx/settings.tsx
+++ src/pages/maimaidx/settings.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "@/components/common/header";
+import MaimaiDxVersionManager from "@/components/mai2/version-management";
 
 interface GameSettingsProps {
 	onUpdate?: () => void;
