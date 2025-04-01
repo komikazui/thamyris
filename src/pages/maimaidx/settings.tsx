dc60b19a1d44293e11a3545940787218d47ce9ac
Polaris <tmillerklugman@gmail.com>
2025-04-01 18:05:17 -0400

    updates

--- src/pages/maimaidx/settings.tsx
+++ src/pages/maimaidx/settings.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Header from "@/components/common/header";
-import MaimaiDxVersionManager from "@/components/mai2/version-management";
+import MaimaiDxVersionManager from "@/components/settings/maimaidx/version-management";
 
 interface GameSettingsProps {
 	onUpdate?: () => void;
