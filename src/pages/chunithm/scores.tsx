9a0f13529e3f51d0e7494f1fcb589ec52c201c2a
PolarisPyra <your-email@example.com>
2025-04-13 11:49:11 -0400

    forgot

--- src/pages/chunithm/scores.tsx
+++ src/pages/chunithm/scores.tsx
@@ -44,7 +44,7 @@ const ChunithmScorePage = () => {
 				<img
 					width={40}
 					height={40}
-					src={`${cdnUrl}/assets/jacket/${row.jacketPath?.replace(".dds", ".png")}`}
+					src={`${cdnUrl}/jacket/${row.jacketPath?.replace(".dds", ".png")}`}
 					alt={row.title}
 					className="flex-shrink-0"
 				/>
