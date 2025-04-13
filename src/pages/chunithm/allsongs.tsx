fd21291b9ae26501928d628b00068d262f563fe1
PolarisPyra <your-email@example.com>
2025-04-13 11:44:34 -0400

    update

--- src/pages/chunithm/allsongs.tsx
+++ src/pages/chunithm/allsongs.tsx
@@ -41,7 +41,7 @@ const ChunithmAllSongs = () => {
 				<img
 					width={40}
 					height={40}
-					src={`${cdnUrl}/assets/jacket/${row.jacketPath?.replace(".dds", ".png")}`}
+					src={`${cdnUrl}/jacket/${row.jacketPath?.replace(".dds", ".png")}`}
 					alt={row.title}
 					className="flex-shrink-0"
 				/>
