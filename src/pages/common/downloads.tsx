3ea70642db04580404347049b2ff254140ccdc5e
PolarisPyra <tmillerklugman@gmail.com>
2025-04-25 10:00:00 -0400

    only admin and hasdownloads can see downloads

--- src/pages/common/downloads.tsx
+++ src/pages/common/downloads.tsx
@@ -8,16 +8,16 @@ import { useFiles } from "@/hooks/users/use-files";
 
 const Downloads = () => {
 	const navigate = useNavigate();
-	const { isAdmin, isSpecial, hasDownloads } = useRoles();
+	const { isAdmin, hasDownloads } = useRoles();
 
 	const [currentPath, setCurrentPath] = useState("");
 	const { data = [], isLoading, error } = useFiles(currentPath);
 
 	useEffect(() => {
-		if (!(isAdmin || isSpecial || hasDownloads)) {
+		if (!(isAdmin || hasDownloads)) {
 			navigate("/");
 		}
-	}, [isAdmin, isSpecial, hasDownloads, navigate]);
+	}, [isAdmin, hasDownloads, navigate]);
 
 	const formatFileSize = (bytes: number) => {
 		if (bytes === 0) return "0 Bytes";
