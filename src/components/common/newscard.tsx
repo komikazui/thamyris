97438f72226a13ce8659c98c4b39d9f65df28406
PolarisPyra <tmillerklugman@gmail.com>
2025-04-25 13:29:29 -0400

    change news card

--- src/components/common/newscard.tsx
+++ src/components/common/newscard.tsx
@@ -2,22 +2,19 @@ import React from "react";
 
 interface NewsCardProps {
 	title: string;
-	description: React.ReactNode;
 	date: string;
 	time: string;
 	icon: React.ReactNode;
 }
 
-const NewsCard = ({ title, description, date, time, icon }: NewsCardProps) => {
+const NewsCard = ({ title, date, time, icon }: NewsCardProps) => {
 	return (
 		<div className="bg-card rounded-md p-6">
 			<div className="flex items-start space-x-4">
 				<div className="flex-shrink-0">{icon}</div>
 				<div className="flex-1">
 					<h2 className="text-primary text-xl font-semibold">{title}</h2>
-					<p className="text-primary mt-2">{description}</p>
 					<div className="text-primary mt-4 flex flex-col text-sm">
-						<span>Latest update</span>
 						<span className="mt-1">
 							{date}&nbsp;{time}
 						</span>
--- src/pages/common/server-news.tsx
+++ src/pages/common/server-news.tsx
@@ -8,21 +8,8 @@ import NewsCard from "@/components/common/newscard";
 const newsBulletin = [
 	{
 		id: 1,
-		title: "News",
-		description: (
-			<>
-				Please check the&nbsp;
-				<a
-					href="https://discord.gg/fucFf4v2jb"
-					target="_blank"
-					rel="noopener noreferrer"
-					className="text-blue-500 hover:underline"
-				>
-					Discord
-				</a>
-				&nbsp; for updates
-			</>
-		),
+		title: "Latest Server Update",
+
 		date: `${env.BUILD_DATE_YEAR_MONTH_DAY}`,
 		time: `${env.BUILD_TIME_12_HOUR} UTC`,
 		icon: <Newspaper className="h-6 w-6 text-blue-500" />,
@@ -37,14 +24,7 @@ const ServerNews = () => {
 			<div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
 				<div className="space-y-6">
 					{newsBulletin.map((item) => (
-						<NewsCard
-							key={item.id}
-							title={item.title}
-							description={item.description}
-							date={item.date}
-							time={item.time}
-							icon={item.icon}
-						/>
+						<NewsCard key={item.id} title={item.title} date={item.date} time={item.time} icon={item.icon} />
 					))}
 				</div>
 			</div>
