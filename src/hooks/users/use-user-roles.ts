89d8678b0d1ce8306805bfaa629255040b8f3b1d
PolarisPyra <tmillerklugman@gmail.com>
2025-04-25 22:12:14 -0400

    wtf lol

--- src/hooks/users/use-user-roles.ts
+++ src/hooks/users/use-user-roles.ts
@@ -1,37 +1,20 @@
 import { api } from "@/utils";
 import { useQuery } from "@tanstack/react-query";
 
-interface RoleResponse {
-  upload: number;
-  download: number;
-  special: number;
-}
-
-interface ParsedRoles {
-  upload: number;
-  download: number;
-  special: number;
-}
-
 export const useUserRoles = () => {
   return useQuery({
     queryKey: ["userRoles"],
     queryFn: async () => {
       const response = await api.users.roles.$get();
-      if (!response.ok) {
-        throw new Error();
-      }
-      
-      const data = await response.json() as RoleResponse;
-      
-      // Convert all values to numbers
-      const parsedData: ParsedRoles = {
+      if (!response.ok) throw new Error();
+
+      const data = await response.json();
+
+      return {
         upload: Number(data.upload),
         download: Number(data.download),
-        special: Number(data.special)
+        special: Number(data.special),
       };
-      
-      return parsedData;
     },
   });
 };
